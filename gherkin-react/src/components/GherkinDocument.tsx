0bd87d65fcaabca0278875bb5a5d1ab9d19f1445
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-08-29 23:03:56 +0100

    Render scenarios

--- gherkin-react/src/components/GherkinDocument.tsx
+++ gherkin-react/src/components/GherkinDocument.tsx
@@ -1,9 +1,47 @@
 import * as React from "react";
 import {io} from "cucumber-messages";
 import IGherkinDocument = io.cucumber.messages.IGherkinDocument;
+import IFeature = io.cucumber.messages.IFeature;
+import IScenario = io.cucumber.messages.IScenario;
 
 export interface GherkinProps {
     gherkinDocument: IGherkinDocument
 }
 
-export const GherkinDocument = ({gherkinDocument}: GherkinProps) => <h1>Hello from {gherkinDocument.feature.name}!</h1>;
+export const GherkinDocument = ({gherkinDocument}: GherkinProps) => {
+    if(!gherkinDocument.feature) {
+        return <div>No feature</div>
+    }
+    return <Feature feature={gherkinDocument.feature} />
+};
+
+interface FeatureProps {
+    feature: IFeature
+}
+
+const Feature = ({feature}: FeatureProps) => {
+    return <div>
+        <h2 className="feature"><span className="keyword">{feature.keyword}: </span><span className="name">{feature.name}</span></h2>
+        {feature.children.map((child) => {
+            if (child.scenario) {
+                return <Scenario scenario={child.scenario} />
+            }
+        })}
+    </div>
+};
+
+interface ScenarioProps {
+    scenario: IScenario
+}
+
+const Scenario = ({scenario}: ScenarioProps) => {
+    return <div>
+        <h2 className="feature"><span className="keyword">{scenario.keyword}: </span><span className="name">{scenario.name}</span></h2>
+        <ol>
+            {scenario.steps.map((step) => {
+                return <li><span className="keyword">{step.keyword}</span><span className="text">{step.text}</span></li>
+            })}
+        </ol>
+    </div>
+};
+
