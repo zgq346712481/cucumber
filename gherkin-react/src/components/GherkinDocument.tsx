8836310469beecf240f0ee6afe3cc7f25000607e
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-08-30 22:13:30 +0100

    More elements

--- gherkin-react/src/components/GherkinDocument.tsx
+++ gherkin-react/src/components/GherkinDocument.tsx
@@ -3,6 +3,8 @@ import {io} from "cucumber-messages";
 import styled from 'styled-components';
 import IGherkinDocument = io.cucumber.messages.IGherkinDocument;
 import IFeature = io.cucumber.messages.IFeature;
+import IRule = io.cucumber.messages.IRule;
+import IBackground = io.cucumber.messages.IBackground;
 import IScenario = io.cucumber.messages.IScenario;
 import IStep = io.cucumber.messages.IStep;
 
@@ -41,10 +43,44 @@ const Feature = ({feature}: FeatureProps) => {
     return <div>
         <h2 className="feature"><Keyword>{feature.keyword}</Keyword>: <Name>{feature.name}</Name></h2>
         {feature.children.map((child, index) => {
-            if (child.scenario) {
+            if (child.background) {
+                return <Background key={`background-${index}`} background={child.background}/>
+            } else if (child.scenario) {
+                return <Scenario key={`scenario-${index}`} scenario={child.scenario}/>
+            } else if (child.rule) {
+                return <Rule key={`rule-${index}`} rule={child.rule}/>
+            }
+        })}
+    </div>
+};
+
+interface RuleProps {
+    rule: IRule
+}
+
+const Rule = ({rule}: RuleProps) => {
+    return <div>
+        <h3 className="rule"><Keyword>{rule.keyword}</Keyword>: <Name>{rule.name}</Name></h3>
+        {rule.children.map((child, index) => {
+            if (child.background) {
+                return <Background key={`background-${index}`} background={child.background}/>
+            } else if (child.scenario) {
                 return <Scenario key={`scenario-${index}`} scenario={child.scenario}/>
             }
         })}
+    </div>
+};
+
+interface BackgroundProps {
+    background: IBackground
+}
+
+const Background = ({background}: BackgroundProps) => {
+    return <div>
+        <h3 className="background"><Keyword>{background.keyword}</Keyword>: <Name>{background.name}</Name></h3>
+        <StepList>
+            {background.steps.map((step, index) => <Step key={`step-${index}`} step={step}/>)}
+        </StepList>
     </div>
 };
 
