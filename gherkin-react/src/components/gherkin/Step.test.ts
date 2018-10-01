f4e6d35f3988eb13f608e81d06b102f46e89b0ea
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-10-01 23:29:24 +0100

    Add mocha

---
+++ gherkin-react/src/components/gherkin/Step.test.ts
@@ -0,0 +1,21 @@
+import Step from "./Step";
+import {configure, shallow} from "enzyme";
+import * as React from "react";
+import * as Adapter from 'enzyme-adapter-react-16';
+import {io} from "cucumber-messages";
+import IStep = io.cucumber.messages.IStep;
+
+configure({ adapter: new Adapter() });
+
+describe("<Step />", () => {
+    it("renders without results", () => {
+        const step: IStep = {
+            keyword: "Given ",
+            text: "I have 10 cukes"
+        }
+        console.log(step)
+
+        const e = shallow(React.createElement(Step, {step}, null))
+        console.log(e.html())
+    })
+})
