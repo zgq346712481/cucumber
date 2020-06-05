77c64b19fdff00e678917a8eb195fdb9b1e79d6f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-06-05 14:14:09 +0100

    Small start on #include preprocessor

--- gherkin/go/gherkin-generate-tokens/gherkin-generate-tokens.go
+++ gherkin/go/gherkin-generate-tokens/gherkin-generate-tokens.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/cucumber/gherkin-go/v13"
 	"io"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -78,7 +80,25 @@ func GenerateTokens(in io.Reader, out io.Writer) error {
 	parser.StopAtFirstError(true)
 	matcher := gherkin.NewMatcher(gherkin.GherkinDialectsBuildin())
 
-	scanner := gherkin.NewScanner(in)
+	processedIn := preprocess(in)
+	scanner := gherkin.NewScanner(processedIn)
 
 	return parser.Parse(scanner, matcher)
+}
+
+func preprocess(in io.Reader) io.Reader {
+	re := regexp.MustCompile("\\s*#include\\s+(.*)")
+
+	preprocessed := ""
+	scanner := bufio.NewScanner(in)
+	for scanner.Scan() {
+		matches := re.FindStringSubmatch(scanner.Text())
+		if len(matches) == 0 {
+			preprocessed += scanner.Text()
+			preprocessed += "\n"
+		} else {
+			preprocessed += "      | what |\n      | minimalism |\n"
+		}
+	}
+	return strings.NewReader(preprocessed)
 }
---
+++ gherkin/go/testdata/good/scenario_outline_with_include_directive.feature
@@ -0,0 +1,7 @@
+Feature: Minimal Scenario Outline
+
+  Scenario: minimalistic
+    Given the <what>
+
+    Examples:
+      #include scenario_outline_with_include_directive.csv
